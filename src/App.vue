f8677c5c32147526598ad003220143bcb575a765
Charana Sukroat <97074318+lnwwaveza007@users.noreply.github.com>
2025-01-23 19:50:30 +0700

    012 - [update] Ready to do the project!

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,38 @@
 <script setup>
+  //Kong Start
+  //Kong End
+  //Wave Start
+  //Wave End
+  //Boom Start
+  //Boom End
+  //Chica Start
+  //Chica End
+  //Tonpee Start
+  //Tonpee End
 </script>
 
 <template>
-  <h1 class="">Test</h1>
+  <!-- Kong Start -->
+  <!-- Kong End -->
+  <!-- Wave Start -->
+  <!-- Wave End -->
+  <!-- Boom Start -->
+  <!-- Boom End -->
+  <!-- Chica Start -->
+  <!-- Chica End -->
+  <!-- Tonpee Start -->
+  <!-- Tonpee End -->
 </template>
 
 <style scoped>
-
+  /* Kong Start */
+  /* Kong End */
+  /* Wave Start */
+  /* Wave End */
+  /* Boom Start */
+  /* Boom End */
+  /* Chica Start */
+  /* Chica End */
+  /* Tonpee Start */
+  /* Tonpee End */
 </style>
