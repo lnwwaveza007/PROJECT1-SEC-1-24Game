2c7f46e8dc05cac6605c59dace16b5146deb7b23
KONGPHOB KONGSAN <kongphob.kong@kmutt.ac.th>
2025-01-30 12:03:06 +0700

    004-[add] Add isSelectedNumbers and isEquationValid functions, refactor message logic to use computed and watch properties

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,48 @@ const numbers = ref([]);
 const equation = ref("");
 const selectedNumbers = ref([]);
 
+//ตรวจสอบว่าตัวเลขถูกเลือกหรือยัง
+const isSelectedNumbers = computed(() => {
+  return (index) => selectedNumbers.value.includes(index)
+})
+
+//ตรวจสอบสมการว่าถูกต้องไหม
+const isEquationValid = computed(() => {
+  try{
+    eval(equation.value)
+    return selectedNumbers.value.length > 0 // เลือกตัวเลขอย่างน้อย 1 ตัว
+  }catch{
+    return false
+  }
+})
+
+//สร้าง message
+const message = computed(() => {
+  if(selectedNumbers.value.length === 0 && equation.value.length ===0){
+    return "" //start
+  }
+  try{
+    const result = eval(equation.value)
+    if(result === 24){
+      return "Correct! The result is 24!"
+    }else{
+      return `Incorrect your result is ${result} , please try again`
+    }
+  }catch{ 
+    return "Invalid equation! Please check your input"
+  }
+})
+
+// watcher ตรวจสอบ selectedNumbers
+watch(selectedNumbers,(newValue,OldValue) => {
+  if(newValue.length > 4){
+    alert("Error: You can select up to 4 number")
+    selectedNumbers.value = OldValue
+    equation.value = ""
+  }
+}, {
+  deep:true
+})
 const generateNumbers = () => {
   numbers.value = [];
   for (let i = 0; i < 4; i++) {
